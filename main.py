1f357b832629c857d7b687900356d72188cfa0e2
proficod <95757010+proficod@users.noreply.github.com>
2025-11-19 16:03:27 +0100

    vol.1

--- main.py
+++ main.py
@@ -1,22 +1,89 @@
-# modul pro práci s pseudo-náhodnými procesy    
 import random
-# modul pro praci se stopky
 import time
 
-welcome_text = ('''
-Hi there!
+# defs
+# Random secret number
+def make_secret():
+    secret = ""
+    digits = "0123456789"
+    while len(secret) < 4:
+        d = random.choice(digits)
+        if len(secret) == 0 and d == "0":
+            continue
+        if d not in secret:
+            secret += d
+    return secret
+
+# Check if all rules are followed
+def check_input(guess):
+    if len(guess) != 4:
+        print("Enter exactly 4 digits\n")
+        return False
+    if not guess.isdigit():
+        print("Enter only numbers.\n")
+        return False
+    if len(set(guess)) != 4:
+        print("Each number should appear only once.\n")
+        return False
+    if guess[0] == "0":
+        print("The number should not start from 0.\n")
+        return False
+    return True
+
+# Count of bulls and cows
+def get_bulls_and_cows(secret, guess):
+    bulls = 0
+    cows = 0
+
+    for i in range(4):
+        if guess[i] == secret[i]:
+            bulls += 1
+        elif guess[i] in secret:
+            cows += 1
+
+    return bulls, cows
+
+# Одна игра
+def play_game():
+    print('''Hi there!
 -----------------------------------------------
 I've generated a random 4 digit number for you.
 Let's play a bulls and cows game.
 -----------------------------------------------
 Enter a number:
 -----------------------------------------------''')
-print(welcome_text)
+    
+    secret = make_secret()
+    attempts = 0
 
-def random_number():
-    number = random.randint(1023, 9876)
-    return number
+    start_time = time.time()
 
-number = random_number
-print(number)
+    while True:
+        guess = input("Your number: ")
 
+        if not check_input(guess):
+            continue
+
+        attempts += 1
+        bulls, cows = get_bulls_and_cows(secret, guess)
+
+        print("Bulls", bulls, "| Cows:", cows, "\n")
+
+        if bulls == 4:
+            end_time = time.time()
+            total = end_time - start_time
+            print("Congrats, you found out the number:", secret)
+            print("Attempts:", attempts)
+            print("Total time:", round(total, 1), "seconds\n")
+            break
+
+# ------------------------------
+# Repeat Game Cycle
+# ------------------------------
+
+while True:
+    play_game()
+    again = input("Wanna play again? (y/n): ").lower()
+    if again != "y":
+        print("Thanks for playing! Byyyyyee!")
+        break
